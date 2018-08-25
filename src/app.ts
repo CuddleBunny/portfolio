2770ff342bea42834b4b7395cd7affec8869e9ae
Josh Dovishaw <cuddlebunny3@live.com>
2018-08-25 13:05:45 -0400

    set up router

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+import { PLATFORM } from 'aurelia-pal';
+import { RouterConfiguration, Router, NavigationInstruction } from 'aurelia-router';
+import { FluentLiveTile } from 'fluent/elements/live-tiles/fluent-live-tile';
+
 export class App {
-  message = 'Hello World!';
+	router: Router;
+
+	configureRouter(config: RouterConfiguration, router: Router): void {
+		this.router = router;
+		config.title = 'Portfolio | Josh Dovishaw';
+		config.map([
+			{ route: [''], name: 'Start', moduleId: PLATFORM.moduleName('views/start') }
+		]);
+	}
 }
