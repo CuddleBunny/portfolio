918f2e7a2c0bb1960d720fc41492ac81e414f7a7
Josh Dovishaw <cuddlebunny3@live.com>
2018-09-10 14:56:03 -0400

    asp.net core 2.1 and new icon

--- Program.cs
+++ Program.cs
@@ -12,12 +12,11 @@ namespace CuddleBunnyPortfolio
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>();
     }
 }
