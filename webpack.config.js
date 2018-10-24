8a816d5eb33f9fa39717147ae2265709aca2961d
Josh Dovishaw <cuddlebunny3@live.com>
2018-10-24 13:19:29 -0400

    fix app insights error

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = ({ production, server, extractCss, coverage, analyze, karma } =
 	},
 	entry: {
 		app: ['aurelia-bootstrapper'],
-		vendor: ['bluebird', 'whatwg-fetch', 'applicationinsights-js/dist/ai.js' ],
+		vendor: ['bluebird', 'whatwg-fetch' ],
 	},
 	mode: production ? 'production' : 'development',
 	output: {
